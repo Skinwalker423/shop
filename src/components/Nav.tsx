dfa657ea87d38d569860a68e6fd4a19ad8e59e58
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-18 19:57:21 -0700

    NavLink

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,35 @@
-import React from "react";
+"use client";
 
-export const Nav = () => {
-  return <nav>Nav</nav>;
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { ComponentProps } from "react";
+
+export const Nav = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <nav className='bg-primary text-primary-foreground flex justify-center px-4'>
+      {children}
+    </nav>
+  );
 };
+
+export function NavLink(
+  props: Omit<ComponentProps<typeof Link>, "className">
+) {
+  const pathname = usePathname();
+
+  return (
+    <Link
+      className={cn(
+        "p-4 hover:bg-secondary hover:text-secondary-foreground focus:bg-secondary focus:text-secondary-foreground",
+        pathname === props.href &&
+          "bg-background text-foreground"
+      )}
+      {...props}
+    />
+  );
+}
