a42e786cf984c0f8ee42efc35b6f51518f86602d
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-21 18:39:33 -0700

    product page header and button

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { PageHeader } from "../_components/PageHeader";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  return (
+    <>
+      <div className='flex justify-between items-center gap-4'>
+        <PageHeader>Admin Products Page</PageHeader>
+        <Button asChild>
+          <Link href={"/admin/product/new"}>
+            Add Product
+          </Link>
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default ProductsPage;
