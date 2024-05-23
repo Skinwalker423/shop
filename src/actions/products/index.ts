cf91cb047ca663e1e6f423d2b3475fa2ee8dd661
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-22 22:24:19 -0700

    add product

--- src/actions/products/index.ts
+++ src/actions/products/index.ts
@@ -27,3 +27,19 @@ export const getAllProducts = async () => {
   const data = await db.product.findMany({});
   return data;
 };
+
+export const addProduct = async (formData: FormData) => {
+  const name = formData.get("name")?.toString();
+  if (!name) return;
+
+  console.log("form data", formData);
+  console.log("name", name);
+
+  // const data = await db.product.create({
+  //   data: {
+  //     name,
+
+  //   }
+  // });
+  // return data;
+};
