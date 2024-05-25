09b787daf96151f087fecd056620b8c97bcbe2d0
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-24 17:38:40 -0700

    products page refactor

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,23 +1,10 @@
 import Link from "next/link";
 
-import { EllipsisVertical } from "lucide-react";
 import { PageHeader } from "../_components/PageHeader";
 import { Button } from "@/components/ui/button";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { getAllProducts } from "@/actions/products";
-import { formatCurrency } from "@/lib/formatter";
+import { ProductsTable } from "./_components/ProductsTable";
 
 const ProductsPage = async () => {
-  const products = await getAllProducts();
-
   return (
     <>
       <div className='flex justify-between items-center gap-4'>
@@ -28,52 +15,7 @@ const ProductsPage = async () => {
           </Link>
         </Button>
       </div>
-      <Table>
-        <TableCaption>
-          A list of your recent invoices.
-        </TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className='w-0'>
-              <span className='sr-only'>
-                Available for purchase
-              </span>
-            </TableHead>
-            <TableHead className=''>Name</TableHead>
-            <TableHead>Price</TableHead>
-            <TableHead>Orders</TableHead>
-            <TableHead className='w-0'>
-              <span className='sr-only'>Actions</span>
-            </TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {products.length > 0 &&
-            products.map((product) => {
-              return (
-                <TableRow key={product.id}>
-                  <TableCell className='font-medium'>
-                    {product.isAvailableForPurchase
-                      ? "✅"
-                      : "❎"}
-                  </TableCell>
-                  <TableCell className='font-medium'>
-                    {product.name}
-                  </TableCell>
-                  <TableCell>
-                    {formatCurrency(product.priceInCents)}
-                  </TableCell>
-                  <TableCell>{product.id}</TableCell>
-                  <TableCell className='font-medium'>
-                    <Button variant={"outline"} size={"sm"}>
-                      <EllipsisVertical />
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-        </TableBody>
-      </Table>
+      <ProductsTable />
     </>
   );
 };
