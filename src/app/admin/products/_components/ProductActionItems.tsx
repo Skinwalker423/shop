ea37f00965f9b6ad72d3808b067454a7ae7e9438
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-25 19:23:56 -0700

    ActiveToggleDropdownItem

---
+++ src/app/admin/products/_components/ProductActionItems.tsx
@@ -0,0 +1,32 @@
+import { toggleProductAvailability } from "@/actions/products";
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import React, { useTransition } from "react";
+
+interface ActiveToggleDropdownItemProps {
+  id: string;
+  isAvailableForPurchase: boolean;
+}
+
+export const ActiveToggleDropdownItem = ({
+  id,
+  isAvailableForPurchase,
+}: ActiveToggleDropdownItemProps) => {
+  const [isPending, startTransition] = useTransition();
+
+  return (
+    <DropdownMenuItem
+      onClick={() => {
+        startTransition(async () => {
+          await toggleProductAvailability(
+            id,
+            !isAvailableForPurchase
+          );
+        });
+      }}
+    >
+      {isAvailableForPurchase
+        ? "Deactivate Product"
+        : "Activate Product"}
+    </DropdownMenuItem>
+  );
+};
