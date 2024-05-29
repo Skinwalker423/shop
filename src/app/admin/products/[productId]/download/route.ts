dba5261d67ba413e15091a2a02019eededd15b8a
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-28 21:13:22 -0700

    download file api

--- src/app/admin/products/[productId]/download/route.ts
+++ src/app/admin/products/[productId]/download/route.ts
@@ -1,8 +1,40 @@
 import { NextRequest, NextResponse } from "next/server";
+import db from "../../../../../../db";
+import { notFound } from "next/navigation";
+import fs from "fs/promises";
 
 export const dynamic = "force-dynamic"; // defaults to auto
-export async function GET(request: NextRequest) {
-  console.log("req", request.nextUrl.pathname);
+export async function GET(
+  request: NextRequest,
+  {
+    params: { productId },
+  }: { params: { productId: string } }
+) {
+  console.log("params", productId);
 
-  return NextResponse.json({ message: "test" });
+  const product = await db.product.findUnique({
+    where: { id: productId },
+    select: {
+      filePath: true,
+      name: true,
+    },
+  });
+
+  if (product == null || !product.filePath)
+    return notFound();
+
+  const { size } = await fs.stat(product.filePath);
+  const file = await fs.readFile(product.filePath);
+  const extension = product.filePath.split(".").pop();
+
+  if (!file) return notFound();
+
+  return new NextResponse(file, {
+    status: 200,
+    statusText: "OK",
+    headers: {
+      "Content-Disposition": `attachment; filename="${product.name}.${extension}"`,
+      "Content-Length": size.toString(),
+    },
+  });
 }
