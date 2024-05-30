258bd362e48e8ab5839f945ad9c54868d66c7c56
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-29 19:07:27 -0700

    isAuthenticated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,42 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  const newUrl = new URL("/", request.url);
+export async function middleware(request: NextRequest) {
+  const newUrl = new URL("http://localhost:3000");
   console.log("url", request.url);
+  if ((await isAuthenticated(request)) === false) {
+    console.log("redirecting to basic auth");
+    return new NextResponse("Unauthorized", {
+      status: 401,
+      headers: {
+        "WWW-Authenticate": "Basic",
+      },
+    });
+  }
   return NextResponse.next();
 }
+
+const isAuthenticated = async (req: NextRequest) => {
+  const authHeader =
+    req.headers.get("authorization") ||
+    req.headers.get("Authorization");
+  console.log("headers", authHeader);
+
+  if (!authHeader) return false;
+
+  const [username, password] = Buffer.from(
+    authHeader?.split(" ")[1],
+    "base64"
+  )
+    .toString()
+    .split(":");
+  console.log("buffer", username, password);
+
+  if (authHeader === "admin") return true;
+
+  return false;
+};
 
 // See "Matching Paths" below to learn more
 export const config = {
