a6a1556bcd605e959c39b589ba6befa4631ec6d3
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-01 19:57:15 -0700

    products page

--- src/app/(customerFacing)/products/page.tsx
+++ src/app/(customerFacing)/products/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { getAllAvailableProducts } from "@/actions/products";
+import { ProductCardSkeleton } from "@/components/products/ProductSkeleton";
+import { ProductsList } from "@/components/products/ProductsList";
+import React, { Suspense } from "react";
 
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+      <Suspense
+        fallback={
+          <>
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+          </>
+        }
+      >
+        <ProductsList
+          productFetcher={getAllAvailableProducts}
+        />
+      </Suspense>
+    </div>
+  );
 };
 
 export default ProductsPage;
