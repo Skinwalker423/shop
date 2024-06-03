5e03daf787f8c5ac894fbd38d7d472d194d70ed5
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-02 21:26:29 -0700

    not found

--- src/app/(customerFacing)/products/[productId]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[productId]/purchase/page.tsx
@@ -1,12 +1,18 @@
+import { getProductById } from "@/actions/products";
+import { notFound } from "next/navigation";
 import React from "react";
 
-const PurchasePage = ({
+const PurchasePage = async ({
   params,
 }: {
   params: { productId: string };
 }) => {
   console.log("params", params);
-  return <div>PurchasePage</div>;
+  const product = await getProductById(params.productId);
+
+  if (product == null) return notFound();
+
+  return <div>PurchasePage {product.name}</div>;
 };
 
 export default PurchasePage;
