dab9b349c04360ba215091a46525cee6667607cd
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-03 19:37:41 -0700

    payment intent

--- src/app/(customerFacing)/products/[productId]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[productId]/purchase/page.tsx
@@ -2,6 +2,12 @@ import { getProductById } from "@/actions/products";
 import { notFound } from "next/navigation";
 import React from "react";
 
+import Stripe from "stripe";
+
+const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY as string
+);
+
 const PurchasePage = async ({
   params,
 }: {
@@ -12,7 +18,24 @@ const PurchasePage = async ({
 
   if (product == null) return notFound();
 
-  return <div>PurchasePage {product.name}</div>;
+  const paymentIntent = await stripe.paymentIntents.create({
+    currency: "USD",
+    amount: product.priceInCents,
+    metadata: {
+      productId: product.id,
+    },
+  });
+
+  if (paymentIntent.client_secret == null) {
+    throw Error("Stripe failed to create payment intent");
+  }
+
+  return (
+    <CheckoutForm
+      product={product}
+      clientSecret={paymentIntent.client_secret}
+    />
+  );
 };
 
 export default PurchasePage;
