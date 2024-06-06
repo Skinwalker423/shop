9c8d8ae886fd091a2c165e8470c064133767a6db
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-05 21:33:02 -0700

    success page display product

--- src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+import Stripe from "stripe";
 
-const PurchaseSuccessPage = () => {
-  return <div>PurchaseSuccessPage</div>;
+import { formatCurrency } from "@/lib/formatter";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import { getProductById } from "@/actions/products";
+
+const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY as string
+);
+const PurchaseSuccessPage = async ({
+  searchParams,
+}: {
+  searchParams: {
+    payment_intent: string;
+    payment_intent_client_secret: string;
+    redirect_status: string;
+  };
+}) => {
+  console.log("params", searchParams);
+  const clientSecret =
+    searchParams.payment_intent_client_secret;
+  const payementIntentID = searchParams.payment_intent;
+  const paymentIntent =
+    await stripe.paymentIntents.retrieve(payementIntentID);
+
+  const productId = paymentIntent.metadata.productId;
+
+  if (productId == null) return notFound();
+
+  const product = await getProductById(productId);
+
+  if (product == null) return notFound();
+
+  const isSuccess = paymentIntent.status === "succeeded";
+
+  return (
+    <div className='max-w-5xl mx-auto w-full space-y-8'>
+      <div className='flex gap-4 items-center'>
+        <div className='aspect-video w-1/3 flex-shrink-0 relative'>
+          <Image
+            src={product.imagePath}
+            alt={`product image of ${product.name}`}
+            fill
+            className='object-cover'
+          />
+        </div>
+        <div>
+          <div className='text-lg'>
+            {formatCurrency(product.priceInCents / 100)}
+          </div>
+          <h1 className='text-2xl font-bold'>
+            {product.name}
+          </h1>
+          <div className='line-clamp-3 text-muted-foreground'>
+            {product.description} df wefwe wef wefw efwef
+            wefwef wfwfwwef
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PurchaseSuccessPage;
