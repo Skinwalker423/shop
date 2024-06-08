1b5cf1dc202942b7e76b38c7e4104059e5a34bee
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-07 22:18:28 -0700

    createOrderAndUpsertUser

--- src/actions/orders/index.ts
+++ src/actions/orders/index.ts
@@ -5,13 +5,13 @@ import db from "../../../db";
 export const getSalesData = async () => {
   const data = await db.order.aggregate({
     _count: true,
-    _sum: { pricePaidInCens: true },
+    _sum: { pricePaidInCents: true },
   });
 
   console.log("res", data);
 
   return {
-    amount: data._sum.pricePaidInCens || 0,
+    amount: data._sum.pricePaidInCents || 0,
     count: data._count,
   };
 };
@@ -35,4 +35,34 @@ export const userOrderExists = async (
   console.log("order", order);
 
   return !!order;
+};
+
+interface CreateOrderProps {
+  productId: string;
+  email: string;
+  pricePaidInCents: number;
+}
+
+export const createOrderAndUpsertUser = async ({
+  productId,
+  email,
+  pricePaidInCents,
+}: CreateOrderProps) => {
+  const userFields = {
+    email,
+    orders: {
+      create: {
+        pricePaidInCents,
+        productId,
+      },
+    },
+  };
+
+  const updatedUserOrder = await db.user.upsert({
+    where: {
+      email,
+    },
+    create: userFields,
+    update: userFields,
+  });
 };
