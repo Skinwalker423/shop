ca1abf293832ebd628e03622c91467cd8041d243
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-07 22:21:36 -0700

    upsert user

--- src/app/webhooks/stripe/route.ts
+++ src/app/webhooks/stripe/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
+import { getProductById } from "@/actions/products";
+import { createOrderAndUpsertUser } from "@/actions/orders";
 
 const stripe = new Stripe(
   process.env.STRIPE_SECRET_KEY as string
@@ -17,6 +19,28 @@ export async function POST(request: NextRequest) {
   );
 
   if (event.type === "charge.succeeded") {
+    console.log("charge successfully completed");
+    const chargeSucceeded = event.data.object;
+    const productId = chargeSucceeded.metadata.productId;
+    const email = chargeSucceeded.billing_details.email;
+    const pricePaidInCents = chargeSucceeded.amount;
+
     // create order
+
+    const product = await getProductById(productId);
+
+    if (product == null || email == null) {
+      return new NextResponse("Bad Request", {
+        status: 400,
+      });
+    }
+
+    const userOrder = await createOrderAndUpsertUser({
+      email,
+      pricePaidInCents,
+      productId,
+    });
+
+    console.log("order", userOrder);
   }
 }
