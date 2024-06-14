38f0bc716eaba089e1f183e45de87199a35de53e
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-13 20:26:24 -0700

    preview props imagePath

--- emails/PurchaseReceiptEmail.tsx
+++ emails/PurchaseReceiptEmail.tsx
@@ -14,6 +14,7 @@ import { OrderInformation } from "./components/OrderInformation";
 interface PurchaseReceiptEmailProps {
   product: {
     name: string;
+    imagePath: string;
   };
   order: {
     id: string;
@@ -24,7 +25,11 @@ interface PurchaseReceiptEmailProps {
 }
 
 PurchaseReceiptEmail.PreviewProps = {
-  product: { name: "John Wick" },
+  product: {
+    name: "John Wick",
+    imagePath:
+      "/products/2593f051-3e82-4fc9-81cd-55a9400ce840-666.PNG",
+  },
   order: {
     id: "123",
     createdAt: new Date(),
