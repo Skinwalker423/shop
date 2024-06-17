48d72836a2691146749cd672bb893b056bb0aa45
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-06-16 21:02:33 -0700

    order page form

--- src/app/(customerFacing)/orders/page.tsx
+++ src/app/(customerFacing)/orders/page.tsx
@@ -1,7 +1,43 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 import React from "react";
+import { SubmitButton } from "./_components/SubmitButton";
 
 const OrdersPage = () => {
-  return <div>OrdersPage</div>;
+  return (
+    <form action=''>
+      <Card>
+        <CardHeader>
+          <CardTitle>My Orders</CardTitle>
+          <CardDescription>
+            Enter your email and we will email you your
+            order history and download links
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Label htmlFor='email'>Email</Label>
+          <Input
+            type='email'
+            required
+            name='email'
+            id='email'
+          />
+        </CardContent>
+        <CardFooter>
+          <SubmitButton />
+        </CardFooter>
+      </Card>
+    </form>
+  );
 };
 
 export default OrdersPage;
